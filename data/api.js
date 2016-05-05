beac679799efb79b66ce946ef32a8bd86818ce4b
Ben Fletcher <bjfletcher@gmail.com>
2016-05-05 22:27:54 +0100

    send the article ID as well

--- data/api.js
+++ data/api.js
@@ -1,33 +1,29 @@
+function post (url, emailAddresses, articleId) {
+	return fetch(url, {
+		credentials: 'same-origin',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			contentUUID: articleId,
+			recipients: emailAddresses
+		})
+	})
+}
+
 export default class {
 
 	creditInfo () {
 		return fetch('/rtcl-email/actions/credits', { credentials: 'same-origin' })
 	}
 
-	gift (emailAddresses) {
-		return fetch('/rtcl-email/actions/gift', {
-			credentials: 'same-origin',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				recipients: emailAddresses
-			})
-		})
+	gift (emailAddresses, articleId) {
+		return post('/rtcl-email/actions/gift', emailAddresses, articleId)
 	}
 
-	nonGift (emailAddresses) {
-		return fetch('/rtcl-email/actions/send', {
-			credentials: 'same-origin',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				recipients: emailAddresses
-			})
-		})
+	nonGift (emailAddresses, articleId) {
+		return post('/rtcl-email/actions/send', emailAddresses, articleId)
 	}
 
 }
