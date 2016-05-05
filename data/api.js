117969c8327b53fbfa3c4a20b02dc13cd02f0a9d
Ben Fletcher <bjfletcher@gmail.com>
2016-05-05 18:21:20 +0100

    add origin credentials to the fetch calls

--- data/api.js
+++ data/api.js
@@ -1,15 +1,15 @@
 export default class {
 
 	creditInfo () {
-		return fetch('/rtcl-email/actions/credits')
+		return fetch('/rtcl-email/actions/credits', { credentials: 'same-origin' })
 	}
 
 	gift () {
-		return fetch('/rtcl-email/actions/gift')
+		return fetch('/rtcl-email/actions/gift', { credentials: 'same-origin' })
 	}
 
 	nonGift () {
-		return fetch('/rtcl-email/actions/send')
+		return fetch('/rtcl-email/actions/send', { credentials: 'same-origin' })
 	}
 
 }
