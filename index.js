bdc42eaeb9d6217c820bb8e4079f8da7c52fd518
Ben Fletcher <bjfletcher@gmail.com>
2016-05-10 10:23:00 +0100

    export the mode constants

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import EmailArticleData from './data';
+import { modes as emailArticleModes } from './data/constants';
 import EmailArticleView from './components';
 
-export { EmailArticleData, EmailArticleView };
+export { EmailArticleData, emailArticleModes, EmailArticleView };
