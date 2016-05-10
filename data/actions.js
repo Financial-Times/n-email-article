7310bbf9d78c9f996e64e4c37c1aba257008a419
Ben Fletcher <bjfletcher@gmail.com>
2016-05-10 13:22:36 +0100

    better email validation regex

--- data/actions.js
+++ data/actions.js
@@ -3,7 +3,7 @@ import { actions as constants } from './constants'
 import nNotification from 'n-notification'
 
 function validateEmail (email) {
-	return !email.match(/^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$/)
+	return !email.match(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i)
 }
 
 export default class {
