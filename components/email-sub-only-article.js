76d74f8cf47c54624bc6bcc1e75c873e2cdb9bb8
Ben Fletcher <bjfletcher@gmail.com>
2016-05-12 22:57:27 +0100

    slower expanison and collapse

--- components/email-sub-only-article.js
+++ components/email-sub-only-article.js
@@ -6,21 +6,23 @@ export default ({ isReady, isOpen,
 		emailAddresses, emailAddressErrors, onEmailAddressChange, onAddEmailAddress, onRemoveEmailAddress,
 		onSend, isSending, onClose }) => (
 	<div className={`email-article email-article--${isReady && isOpen ? 'open' : 'closed'}`}>
-		<button onClick={onClose} type="button" className="o-buttons o-buttons--medium email-article__close"><i>Close</i></button>
-		<form noValidate onSubmit={(e) => { e.preventDefault(); onSend() }} className="email-article__container">
-			<h1 className="email-article__title">Email this subscriber-only article</h1>
-			<EmailAddressList
-					items={emailAddresses}
-					errors={emailAddressErrors}
-					onItemChange={onEmailAddressChange}
-					onAdd={onAddEmailAddress}
-					onRemove={onRemoveEmailAddress}
-					showMaySub={true}
-			/>
-			<button type="submit" disabled={isSending} className="email-article__submit o-buttons o-buttons--standout o-buttons--big">Send article</button>
-			<div className="email-article__footnote">
-				This email will show up in their inbox from the Financial Times
-			</div>
-		</form>
+		<div className="email-article__border">
+			<button onClick={onClose} type="button" className="o-buttons o-buttons--medium email-article__close"><i>Close</i></button>
+			<form noValidate onSubmit={(e) => { e.preventDefault(); onSend() }} className="email-article__container">
+				<h1 className="email-article__title">Email this subscriber-only article</h1>
+				<EmailAddressList
+						items={emailAddresses}
+						errors={emailAddressErrors}
+						onItemChange={onEmailAddressChange}
+						onAdd={onAddEmailAddress}
+						onRemove={onRemoveEmailAddress}
+						showMaySub={true}
+				/>
+				<button type="submit" disabled={isSending} className="email-article__submit o-buttons o-buttons--standout o-buttons--big">Send article</button>
+				<div className="email-article__footnote">
+					This email will show up in their inbox from the Financial Times
+				</div>
+			</form>
+		</div>
 	</div>
 );
--- components/email-sub-or-gift-article.js
+++ components/email-sub-or-gift-article.js
@@ -7,29 +7,31 @@ export default ({ isReady, isOpen, isGift, onIsGiftChange, credit,
 		emailAddresses, emailAddressErrors, onEmailAddressChange, onAddEmailAddress, onRemoveEmailAddress,
 		onSend, isSending, onClose }) => (
 	<div className={`email-article email-article--${isReady && isOpen ? 'open' : 'closed'}`}>
-		<button onClick={onClose} type="button" className="o-buttons o-buttons--medium email-article__close"><i>Close</i></button>
-		<form noValidate onSubmit={(e) => { e.preventDefault(); onSend() }} className="email-article__container">
-			<h1 className="email-article__title">Email this subscriber-only article</h1>
-			<GiftOrNotSelection
-					isGift={isGift}
-					onIsGiftChange={onIsGiftChange}
-					credit={credit}
-			/>
-			<div className="email-article__section-break"></div>
-			<EmailAddressList
-					items={emailAddresses}
-					errors={emailAddressErrors}
-					onItemChange={onEmailAddressChange}
-					onAdd={onAddEmailAddress}
-					onRemove={onRemoveEmailAddress}
-					isGift={isGift}
-					credit={credit}
-					showMaySub={true}
-			/>
-			<button type="submit" disabled={isSending} className="email-article__submit o-buttons o-buttons--standout o-buttons--big">Send article</button>
-			<div className="email-article__footnote">
-				This email will show up in their inbox from the Financial Times
-			</div>
-		</form>
+		<div className="email-article__border">
+			<button onClick={onClose} type="button" className="o-buttons o-buttons--medium email-article__close"><i>Close</i></button>
+			<form noValidate onSubmit={(e) => { e.preventDefault(); onSend() }} className="email-article__container">
+				<h1 className="email-article__title">Email this subscriber-only article</h1>
+				<GiftOrNotSelection
+						isGift={isGift}
+						onIsGiftChange={onIsGiftChange}
+						credit={credit}
+				/>
+				<div className="email-article__section-break"></div>
+				<EmailAddressList
+						items={emailAddresses}
+						errors={emailAddressErrors}
+						onItemChange={onEmailAddressChange}
+						onAdd={onAddEmailAddress}
+						onRemove={onRemoveEmailAddress}
+						isGift={isGift}
+						credit={credit}
+						showMaySub={true}
+				/>
+				<button type="submit" disabled={isSending} className="email-article__submit o-buttons o-buttons--standout o-buttons--big">Send article</button>
+				<div className="email-article__footnote">
+					This email will show up in their inbox from the Financial Times
+				</div>
+			</form>
+		</div>
 	</div>
 );
