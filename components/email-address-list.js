1d248f2828a3c6a3c650dd4f33abcdfc5d0fcd44
andygnewman <andrew.newman@ft.com>
2017-02-09 11:32:33 +0000

    focus on last email input field

--- components/email-address-list.js
+++ components/email-address-list.js
@@ -1,65 +1,80 @@
 import React from 'react';
 
-function subNote (isGift, showMaySub) {
-	if (!isGift && showMaySub) return (
-		<div className="email-address__note">
-			This recipient may need to be a subscriber to read this content
-		</div>
-	)
-}
+export default class extends React.Component {
 
-function giftNote (items, isGift, credit) {
-	if (isGift) {
-		const remainder = credit - items.length
-		return (
+	constructor (props) {
+		super(props)
+	}
+
+	componentDidUpdate () {
+		this.lastInput.focus();
+	}
+
+	subNote () {
+		if (!this.props.isGift && this.props.showMaySub) return (
 			<div className="email-address__note">
-				You will have <span className="email-address__gift-count">{remainder} gift article{remainder !== 1 ? 's' : ''}</span> remaining
-				this month
+				This recipient may need to be a subscriber to read this content
 			</div>
 		)
 	}
-}
 
-function inputs (items, errors, onItemChange, onAdd, onRemove, isGift, credit) {
-	const maxItems = isGift ? credit : 10
-	return items.map((address, index) => {
-		const error = !errors[index] ? null : (
-			<div className="email-address__error o-forms-errortext">Please enter a valid email</div>
-		)
-		const action = (maxItems === 1 || index + 1 === items.length && index + 1 < maxItems) ? 'add' : 'remove'
-		const button = (
-				<div className="email-address__button">
-					<button type="button" className="o-buttons o-buttons--big"
-									onClick={() => action === 'add' ? onAdd() : onRemove(index)}
-									disabled={maxItems === 1}>
-						<i className={`email-address__button--${action}`}>{action}</i>
-					</button>
+	giftNote () {
+		if (this.props.isGift) {
+			const remainder = this.props.credit - this.props.items.length
+			return (
+				<div className="email-address__note">
+					You will have <span className="email-address__gift-count">{remainder} gift article{remainder !== 1 ? 's' : ''}</span> remaining
+					this month
 				</div>
-		)
-		return (
-				<div key={index} className={`email-address__item o-forms-group ${error ? 'o-forms--error' : ''}`}>
-					<div className="email-address__input-button">
-						<input type="email" className="o-forms-text email-address__input"
-								autoFocus={true}
-								value={address}
-								onChange={event => onItemChange(index, event.target.value)}></input>
-						{button}
+			)
+		}
+	}
+
+	inputs () {
+		const maxItems = this.props.isGift ? this.props.credit : 10
+		return this.props.items.map((address, index) => {
+			const error = !this.props.errors[index] ? null : (
+				<div className="email-address__error o-forms-errortext">Please enter a valid email</div>
+			)
+			const lastInput = index + 1 === this.props.items.length;
+			const action = (maxItems === 1 || lastInput && index + 1 < maxItems) ? 'add' : 'remove'
+			const button = (
+					<div className="email-address__button">
+						<button type="button" className="o-buttons o-buttons--big"
+										onClick={() => action === 'add' ? this.props.onAdd() : this.props.onRemove(index)}
+										disabled={maxItems === 1}>
+							<i className={`email-address__button--${action}`}>{action}</i>
+						</button>
 					</div>
-					{error}
-				</div>
-		)
-	})
-}
+			)
+			return (
+					<div key={index} className={`email-address__item o-forms-group ${error ? 'o-forms--error' : ''}`}>
+						<div className="email-address__input-button">
+							<input type="email" className="o-forms-text email-address__input"
+									ref={(input) => { lastInput ? this.lastInput = input : null; }}
+									value={address}
+									onChange={event => this.props.onItemChange(index, event.target.value)}></input>
+							{button}
+						</div>
+						{error}
+					</div>
+			)
+		})
+	}
 
-export default ({ items, errors, onItemChange, onAdd, onRemove, isGift, credit, showMaySub }) => (
-		<div className="email-address">
-			<div className="email-address__label">
-				Enter recipient’s email address
+	render () {
+		return (
+			<div className="email-address">
+				<div className="email-address__label">
+					Enter recipient’s email address
+				</div>
+				<div className="email-address__list">
+					{this.inputs()}
+				</div>
+				{this.subNote()}
+				{this.giftNote()}
 			</div>
-			<div className="email-address__list">
-				{inputs(items, errors, onItemChange, onAdd, onRemove, isGift, credit)}
-			</div>
-			{subNote(isGift, showMaySub)}
-			{giftNote(items, isGift, credit)}
-		</div>
-)
+		)
+	}
+
+}
