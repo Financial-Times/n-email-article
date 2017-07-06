37a97f8b127587f32e15250ae8a2df9ec9e5f4b9
Arjun Gadhia <arjungadhia@gmail.com>
2017-07-06 15:03:44 +0100

    Fix verify error

--- components/email-sub-or-gift-article.js
+++ components/email-sub-or-gift-article.js
@@ -12,7 +12,7 @@ export default ({ isReady, isOpen, isGift, onIsGiftChange, credit, monthlyAllowa
 		<div className="email-article__border">
 			<button onClick={onClose} type="button" className="o-buttons o-buttons--mono email-article__close"><i>Close</i></button>
 			<form noValidate onSubmit={(e) => { e.preventDefault(); onSend() }} className="email-article__container">
-				<h1 className="email-article__title">{`${image ? 'Email this graphic and a link to this article' :  'Email this subscriber-only article'}`}</h1>
+				<h1 className="email-article__title">{`${image ? 'Email this graphic and a link to this article' : 'Email this subscriber-only article'}`}</h1>
 				<GiftOrNotSelection
 						isGift={isGift}
 						onIsGiftChange={onIsGiftChange}
